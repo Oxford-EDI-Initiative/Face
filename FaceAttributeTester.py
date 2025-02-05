4c1f3cadfb4ae79280b01718e95e4916298de19d
Danieldb5 <balaamdaniel@gmail.com>
2025-02-05 16:33:02 +0000

    FaceAttributeTester.py

---
+++ FaceAttributeTester.py
@@ -0,0 +1,8 @@
+
+#tests for different facial attributes using a binary scale
+#neutral so return 50% male 50% female same for greyscaled races
+
+#can use prebuilt open source libary to test attributes eg FairFace, DeepFaceLab or design our own
+
+def main():
+  pass

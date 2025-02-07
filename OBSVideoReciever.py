b1c2f9278e1350befab9925549dd9d8e3654444b
Danieldb5 <balaamdaniel@gmail.com>
2025-02-07 01:45:59 +0000

    Update OBSVideoReciever.py

--- OBSVideoReciever.py
+++ OBSVideoReciever.py
@@ -1,1 +1,12 @@
-OBS
+#OBS
+import obswebsocket
+from obswebsocket import obsws, requests
+
+# Connect to OBS WebSocket
+ws = obsws("localhost", 4444, "password")  # Default port is 4444, set your password
+ws.connect()
+
+# Switch to a different scene
+ws.call(requests.SetCurrentScene("Deepfake Scene"))
+
+ws.disconnect()
